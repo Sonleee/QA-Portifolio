ef2aa24423f0f57dcebc02580b966fa68e0e4a83
Leonardo Barbosa <cfps-leo@hotmail.com>
2026-04-13 14:47:38 -0300

    feat: adiciona fluxo completo de checkout no Cypress

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -33,3 +33,43 @@ describe('template spec', () => {
     })
   })
 })
+it('Deve adicionar produto ao carrinho e finalizar compra', () => {
+
+  // Login
+  cy.visit('https://www.saucedemo.com/')
+  cy.get('[data-test="username"]').type('standard_user')
+  cy.get('[data-test="password"]').type('secret_sauce')
+  cy.get('[data-test="login-button"]').click()
+
+  // Adicionar produto ao carrinho
+  cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+
+  // Ir para o carrinho
+  cy.get('.shopping_cart_link').click()
+
+  // Validar produto no carrinho
+  cy.contains('Sauce Labs Backpack').should('be.visible')
+
+  // Ir para checkout
+  cy.get('[data-test="checkout"]').click()
+
+  // Preencher dados
+  cy.get('[data-test="firstName"]').type('Teste')
+  cy.get('[data-test="lastName"]').type('QA')
+  cy.get('[data-test="postalCode"]').type('12345')
+
+  // Continuar
+  cy.get('[data-test="continue"]').click()
+
+  // Validar resumo da compra
+  cy.contains('Checkout: Overview').should('be.visible')
+
+  // Validar preço total da compra
+  cy.get('.summary_total_label').should('contain', '$')
+
+  // Finalizar compra
+  cy.get('[data-test="finish"]').click()
+
+  // Validar sucesso
+  cy.contains('Thank you for your order!').should('be.visible')
+})
